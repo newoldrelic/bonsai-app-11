c9f5883c3922967799d943537b539c1b91abf2da
newoldrelic <new.ashdesouza@gmail.com>
2024-12-06 17:36:21 +0000

    trying to fix type definition for 'vite/client'

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -25,5 +26,10 @@ export default defineConfig({
   },
   define: {
     'process.env': {}
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
   }
 });
