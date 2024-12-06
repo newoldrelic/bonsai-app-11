891e11bb396b2fa6ac60939b3bc6e9cca5e6ab61
newoldrelic <new.ashdesouza@gmail.com>
2024-12-06 12:36:12 +0000

    Claude: attempt fix for build issues, add type def

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_DEBUG_LEVELS: string
+    // Add other env variables you use
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
