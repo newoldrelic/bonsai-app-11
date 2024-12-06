383e128978474afd419025b9783971ec52dbd7d2
newoldrelic <new.ashdesouza@gmail.com>
2024-12-06 14:25:25 +0000

    adding netlify-functions to devdependancies

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -17,6 +17,25 @@ interface ImportMeta {
 }
 
 declare module '@netlify/functions' {
+  export type Handler = (event: HandlerEvent, context: Context) => Promise<HandlerResponse>;
+  
+  export interface HandlerEvent {
+    body: string | null;
+    headers: { [header: string]: string | undefined };
+    httpMethod: string;
+    isBase64Encoded: boolean;
+    path: string;
+    queryStringParameters: { [parameter: string]: string | undefined };
+    rawUrl: string;
+  }
+
+  export interface HandlerResponse {
+    statusCode: number;
+    body?: string;
+    headers?: { [header: string]: string | string[] };
+    isBase64Encoded?: boolean;
+  }
+
   export interface Context {
     clientContext: {
       identity: {
